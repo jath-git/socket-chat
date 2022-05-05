aa02d0b1142e040f388c9c0b46531c5f851a859b
Jathurshan Theivikaran <jathu@Jathurshans-MacBook-Pro.local>
2022-05-04 23:36:40 -0400

    add time helper functions

--- helpers.py
+++ helpers.py
@@ -3,9 +3,29 @@ import os
 from math import floor
 from datetime import datetime
 
-PORT = 6000 + int(datetime.now().minute)
+# code for time in format HH:MM AM/PM
+def get_short_time():
+    now = datetime.now()
+    hour = now.hour
+    min = now.minute
+    section = 'AM'
+
+    if hour == 0:
+        hour = 12
+    elif hour == 12:
+        section = 'PM'
+    elif hour > 12:
+        hour -= 12
+        section = 'PM'
+
+    return f'{str(hour).zfill(2)}{str(min).zfill(2)}{section[0]}'
 
 
+def get_long_time():
+    short_time = get_short_time()
+    return f'{"" if short_time[0] == "0" else "1"}{short_time[1]}:{short_time[2:4]} {short_time[4]}M'
+
+# pad string with hyphens, symmetrically
 def header(heading):
     def pad_dash(str):
         total_length = 50
@@ -19,25 +39,12 @@ def header(heading):
 
     print(pad_dash(heading.upper()))
 
-
-def comment(str):
-    write_comment = True
-
-    if write_comment:
-        return
-    print(str)
-
-
+# clear user shell
 def clear_console():
     command = 'cls' if os.name in ('nt', 'dos') else 'clear'
     os.system(command)
 
-
 def get_address():
-    HOST = socket.gethostname()
-    SERVER = socket.gethostbyname(HOST)
+    SERVER = socket.gethostbyname('localhost')
+    PORT = 6002
     return (SERVER, PORT)
-
-
-def display_address(address):
-    return f'{address[0]} ({address[1]})'
