95080ab973fce729fb78f864c19662c6344b3127
Jathurshan Theivikaran <jathu@Jathurshans-MacBook-Pro.local>
2022-05-07 23:17:30 -0400

    use queue service to print

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from constants import *
 from encryption import send_compound, send_simple_message, all_connections_send, send_message, receive_message
 from getpass import getpass
 from Sockets import Sockets
+from Queue import Queue
 
 sockets = Sockets()
 menu_thread = threading.Event()
@@ -13,8 +14,8 @@ disconnected_server = False
 close_server_thread = threading.Event()
 close_server_thread.clear()
 long_time = get_long_time()
-server = create_socket()
 count_connection_creation = 0
+queue = Queue()
 
 ### MENU
 
@@ -23,17 +24,17 @@ def kick_client():
     valid_connections_count = len(sockets.connections)
 
     if valid_connections_count == 0:
-        print(f'[ERROR][{long_time}] No clients in server')
+        queue.push('ERROR', 'No clients in server')
         return True
 
-    header(' client list ')
-    print('[INPUT] Choose a client to kick:')
+    queue.push_simple(pad_dash(' client list '))
+    queue.push('INPUT', 'Choose a client to kick:')
     for i in range(valid_connections_count):
         name = sockets.get_name_from_index(i)
 
-        print(f'[OPTION] {i + 1} - {name}')
+        queue.push_simple(f'[OPTION] {i + 1} - {name}')
 
-    print()
+    queue.push_empty()
     client_index = input('> ')
 
     if client_index.isdigit():
@@ -45,7 +46,7 @@ def kick_client():
         name = sockets.get_name_from_index(client_index)
         return sockets.get_connection(name)
     else:
-        print(f'[ERROR][{long_time}] Client index is not recognized')
+        queue.push('ERROR', 'Client index is not recognized')
         return None
 
 
@@ -62,25 +63,22 @@ def disconnect_all_users():
     global disconnected_server
     disconnected_server = True
 
-    for c in sockets.connections:
-        sockets.remove_socket(c)
-
-    print()
-    print(
-        f'[SUCCESS][{long_time}] All clients have been disconnected')
+    queue.push_empty()
+    queue.push('SUCCESS', 'All clients have been disconnected')
 
 #  menu options only activated from settings when send/receive threads are paused
 def server_menu():
-    header('menu')
-    print("[OPTION] 1 - Kick a client")
-    print("[OPTION] 2 - Disconnect all clients")
-    print("[OPTION] 3 - Send a message to all clients")
-    print("[OPTION] 4 - Shutdown server")
-    print("[OPTION] 5 - Exit menu\n")
+    queue.push_simple(pad_dash('menu'))
+    queue.push_simple("[OPTION] 1 - Kick a client")
+    queue.push_simple("[OPTION] 2 - Disconnect all clients")
+    queue.push_simple("[OPTION] 3 - Send a message to all clients")
+    queue.push_simple("[OPTION] 4 - Shutdown server")
+    queue.push_simple("[OPTION] 5 - Exit menu\n")
+
     option = input('> ')
 
     if option == '1':
-        print()
+        queue.push_empty()
         client_kicked = kick_client()
 
         if client_kicked == None:
@@ -91,21 +89,21 @@ def server_menu():
             if client_kicked in sockets.connections:
                 send_simple_message(client_kicked, DISCONNECT_SERVER)
                 broadcast(sockets.get_name(client_kicked), 'has been kicked out', False)
-            else:
-                send_message(client, RESPONSE, '[ERROR] Client has already left')
     elif option == '2':
         disconnect_all_users()
     elif option == '3':
-        print('[INPUT] Enter message to send:')
+        queue.push_simple('[INPUT] Enter message to send:')
         server_message = input('> ')
 
         if server_message == '':
-            print(f'[ERROR][{long_time}] Message must not be empty\n')
+            queue.push('ERROR', 'Message must not be empty')
+            queue.push_empty()
             server_menu()
 
         restart_clients()
         broadcast('', server_message, False)
-        print(f'[SUCCESS][{long_time}] Message has been sent out\n')
+        queue.push('SUCCESS', 'Message has been sent out')
+        queue.push_empty()
     elif option == '4':
         disconnect_all_users()
         close_server_thread.set()
@@ -113,7 +111,8 @@ def server_menu():
     elif option == '5' or option == '*':
         restart_clients()
     else:
-        print(f'[ERROR][{long_time}] Invalid Input. Choose from 1 to 5\n')
+        queue.push('ERROR', 'Invalid Input. Choose from 1 to 5')
+        queue.push_empty()
         server_menu()
 
 
@@ -121,11 +120,11 @@ def server_menu():
 def restart_clients():
     menu_thread.clear()
     for c in sockets.connections:
-        try:
-            receive_thread = threading.Thread(
-                target=client_observe, args=(c, sockets.get_address(c), True))
-            receive_thread.start()
+        receive_thread = threading.Thread(
+            target=client_observe, args=(c, sockets.get_address(c), True))
+        receive_thread.start()
 
+        try:
             send_message(c, RESPONSE, '[CONTINUE] Server is unpaused')
         except:
             sockets.remove_socket(c)
@@ -147,9 +146,9 @@ def broadcast(name, text, is_message):
 # thread for each client used when something is received to server socket
 def client_observe(connection, address, already_joined):
     name = sockets.get_name(connection)
-
+        
     if not already_joined:
-        print(f'[USER][{long_time}] {name} joined')
+        queue.push('USER', f'{name} joined')
         for c in sockets.connections:
             try:
                 send_compound(c, name, 'joined', get_short_time(), False)
@@ -158,6 +157,7 @@ def client_observe(connection, address, already_joined):
 
     while True:
         if menu_thread.is_set():
+            send_simple_message(connection, PAUSED)
             return
 
         try:
@@ -176,32 +176,27 @@ def client_observe(connection, address, already_joined):
                                  RESPONSE, f'[PRIVATE][{long_time}] {name}: {private_message}')
                     send_message(connection, RESPONSE,
                                  f'[SUCCESS] Private message was sent')
-                    print(
-                        f'[PRIVATE][{long_time}] {private_message} [from {name} to {client_name}] ')
+                    queue.push('PRIVATE', f'{private_message} [from {name} to {client_name}]')
                 except:
-                    print(
-                        f'[ERROR][{long_time}] Private message from {name} failed')
+                    queue.push('ERROR', f'Private message from {name} failed')
                     send_message(connection, RESPONSE,
                                  f'[ERROR][{long_time}] Private message failed')
 
             elif message.type == MESSAGE:
                 if message.text == '':
-                    print(
-                        f'[ERROR][{long_time}] {name} sent empty message')
+                    queue.push('ERROR', f'{name} sent empty message')
                     send_message(connection, RESPONSE,
                                  '[ERROR] Message must not be empty')
                 else:
-                    print(f'[RECEIVED][{long_time}] {name}: {message.text}')
+                    queue.push('RECEIVED', f'{name}: {message.text}')
                     broadcast(name, message.text, True)
             elif message.type == NAME:
                 if message.text == '':
-                    print(
-                        f'[ERROR][{long_time}] {name} renamed empty string')
+                    queue.push('ERROR', f'{name} renamed empty string')
                     send_message(connection, RESPONSE,
                                  f'[ERROR][{long_time}] New name must not be empty')
                 elif sockets.has_name(message.text):
-                    print(
-                        f'[ERROR][{long_time}] {name} renamed to existing name')
+                    queue.push('ERROR', f'{name} renamed to existing name')
                     send_message(connection, RESPONSE,
                                  f'[ERROR][{long_time}] Name already exists')
                 else:
@@ -209,10 +204,10 @@ def client_observe(connection, address, already_joined):
                     name = message.text
                     sockets.change_name(connection, name)
 
-                    print(f'[USER][{long_time}] {old_name} renamed as {name}')
+                    queue.push('USER', f'{old_name} renamed as {name}')
                     broadcast(old_name, f'renamed as {name}', False)
             elif message.type == MENU:
-                print(f'[USER][{long_time}] {name} accessed menu')
+                queue.push('USER', f'{name} accessed menu')
                 send_simple_message(connection, MENU)
             elif message.type == VOID:
                 pass
@@ -229,12 +224,11 @@ def client_observe(connection, address, already_joined):
 
                 all_connections_send(connection, names)
             elif message.type == DISCONNECT_REQUEST:
-                print(f'[USER][{long_time}] {name} is disconnecting')
+                queue.push('USER', f'{name} has disconnected')
                 broadcast(name, 'disconnected', False)
                 send_simple_message(connection, DISCONNECT_REQUEST)
             elif message.type == DISCONNECT_CONFIRM:
                 sockets.remove_socket(connection)
-                connection.close()
 
 # receive thread continuously checks if menu should be activated
 def read_input():
@@ -249,8 +243,9 @@ def read_input():
                 sockets.remove_socket(c)
 
         server_menu()
-        print()
-        header(' chat server ')
+        queue.push_empty()
+        queue.push_simple(pad_dash(' chat server '))
+
 
     if not close_server_thread.is_set():
         read_input()
@@ -266,9 +261,10 @@ def create_socket():
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return server
     except socket.error as err:
-        print(
-            f'[ERROR][{long_time}] Socket creation failed: {str(err)}')
+        queue.push('ERROR', f'Socket creation failed: {str(err)}')
         exit()
+
+server = create_socket()
 
 
 def create_client_connection():
@@ -287,10 +283,9 @@ def create_client_connection():
             sockets.remove_socket(c)
 
     if count_connection_creation == 1:
-        print(f'[UPDATE][{long_time}] 1 Connection has been created')
+        queue.push('UPDATE', 'Connection has been created')
     else:
-        print(
-            f'[UPDATE][{long_time}] {count_connection_creation} Connections have been created')
+        queue.push('UPDATE', f'{count_connection_creation} Connections have been created')
 
     receive_thread = threading.Thread(
         target=client_observe, args=(connection, address, False))
@@ -301,26 +296,24 @@ def create_client_connection():
 
 def boot_server():
     try:
-        server = create_socket()
         address = get_address()
         server.bind(address)
         server.listen()
         clear_console()
-        header('instructions')
-        print(f'[SUCCESS][{long_time}] Server is Running on {address[0]} ({address[1]}')
-        print(
-            f'[CONTROL][{long_time}] Type and enter "*" anytime to access menu\n')
-        header(' chat server ')
+        queue.push_simple(pad_dash('instructions'))
+        queue.push(f'SUCCESS', f'Server is Running on {address[0]} ({address[1]}')
+        queue.push('CONTROL', 'Type and enter "*" anytime to access menu\n')
+        queue.push_simple(pad_dash(' chat server '))
 
-        menu_thread = threading.Thread(target=read_input)
-        menu_thread.start()
+        read_thread = threading.Thread(target=read_input)
+        read_thread.start()
         create_client_connection()
     except socket.error as err:
         if close_server_thread.is_set():
-            print(f'[SUCCESS][{long_time}] Server has been shutdown')
+            queue.push('SUCCESS', 'Server has been shutdown')
         else:
-            print(f'[ERROR][{long_time}] Socket binding failed: {str(err)}')
-            print(f'[FIX][{long_time}] Try changing port number in shared.py')
+            queue.push('ERROR', f'Socket binding failed: {str(err)}')
+            queue.push('FIX', 'Try changing port number in shared.py')
 
 
 boot_server()
