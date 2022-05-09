943ea3a8bb4741ce1840d18a611611e8e88b1426
Jathurshan Theivikaran <jathu@Jathurshans-MacBook-Pro.local>
2022-05-08 23:07:19 -0400

    create database menu and integrate mysql

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 import socket
 import threading
+import sys
 from helpers import *
 from constants import *
 from encryption import send_compound, send_simple_message, all_connections_send, send_message, receive_message
 from getpass import getpass
-from Sockets import Sockets
+
+sys.path.insert(0, './classes')
 from Queue import Queue
+from Sockets import Sockets
+
 
 sockets = Sockets()
 menu_thread = threading.Event()
@@ -27,12 +31,12 @@ def kick_client():
         queue.push('ERROR', 'No clients in server')
         return True
 
-    queue.push_simple(pad_dash(' client list '))
+    queue.push('HEADING', pad_dash(' client list '))
     queue.push('INPUT', 'Choose a client to kick:')
     for i in range(valid_connections_count):
         name = sockets.get_name_from_index(i)
 
-        queue.push_simple(f'[OPTION] {i + 1} - {name}')
+        queue.push('OPTION', f'{i + 1} - {name}')
 
     queue.push_empty()
     client_index = input('> ')
@@ -66,14 +70,40 @@ def disconnect_all_users():
     queue.push_empty()
     queue.push('SUCCESS', 'All clients have been disconnected')
 
+def database_menu():
+    queue.push('HEADING', pad_dash(' database menu '))
+    queue.push('OPTION', '1 - Save server messages')
+    queue.push('OPTION', '2 - Restore server messages')
+    queue.push('OPTION', '3 - Clear server messages')
+    queue.push('OPTION', '4 - Exit database menu')
+    queue.push_empty()
+
+    option = input('> ')
+
+    if option == '1':
+        queue.save()
+        queue.push('DATABASE', 'Server has been saved successfully')
+    elif option == '2':
+        queue.restore()
+    elif option == '3':
+        queue.clear()
+        queue.push('DATABASE', 'Server has been cleared successfully')
+    elif option == '4':
+        pass
+    else:
+        queue.push('ERROR', 'Invalid Input. Choose from 1 to 4')
+        database_menu()
+
 #  menu options only activated from settings when send/receive threads are paused
 def server_menu():
-    queue.push_simple(pad_dash('menu'))
-    queue.push_simple("[OPTION] 1 - Kick a client")
-    queue.push_simple("[OPTION] 2 - Disconnect all clients")
-    queue.push_simple("[OPTION] 3 - Send a message to all clients")
-    queue.push_simple("[OPTION] 4 - Shutdown server")
-    queue.push_simple("[OPTION] 5 - Exit menu\n")
+    queue.push('HEADING', pad_dash('menu'))
+    queue.push('OPTION', '1 - Kick a client')
+    queue.push('OPTION', '2 - Disconnect all clients')
+    queue.push('OPTION', '3 - Send a message to all clients')
+    queue.push('OPTION', '4 - Shutdown server')
+    queue.push('OPTION', '5 - Access database')
+    queue.push('OPTION', '6 - Exit menu')
+    queue.push_empty()
 
     option = input('> ')
 
@@ -92,7 +122,7 @@ def server_menu():
     elif option == '2':
         disconnect_all_users()
     elif option == '3':
-        queue.push_simple('[INPUT] Enter message to send:')
+        queue.push('INPUT', 'Enter message to send:')
         server_message = input('> ')
 
         if server_message == '':
@@ -108,10 +138,12 @@ def server_menu():
         disconnect_all_users()
         close_server_thread.set()
         server.close()
-    elif option == '5' or option == '*':
+    elif option == '5':
+        database_menu()
+    elif option == '6' or option == '*':
         restart_clients()
     else:
-        queue.push('ERROR', 'Invalid Input. Choose from 1 to 5')
+        queue.push('ERROR', 'Invalid Input. Choose from 1 to 6')
         queue.push_empty()
         server_menu()
 
@@ -244,7 +276,7 @@ def read_input():
 
         server_menu()
         queue.push_empty()
-        queue.push_simple(pad_dash(' chat server '))
+        queue.push('HEADING', pad_dash(' chat server '))
 
 
     if not close_server_thread.is_set():
@@ -295,15 +327,20 @@ def create_client_connection():
         create_client_connection()
 
 def boot_server():
+    def get_address():
+        SERVER = socket.gethostbyname('localhost')
+        PORT = 6002
+        return (SERVER, PORT)
+
     try:
         address = get_address()
         server.bind(address)
         server.listen()
         clear_console()
-        queue.push_simple(pad_dash('instructions'))
+        queue.push('HEADING', pad_dash(' instructions '))
         queue.push(f'SUCCESS', f'Server is Running on {address[0]} ({address[1]}')
         queue.push('CONTROL', 'Type and enter "*" anytime to access menu\n')
-        queue.push_simple(pad_dash(' chat server '))
+        queue.push('HEADING', pad_dash(' chat server '))
 
         read_thread = threading.Thread(target=read_input)
         read_thread.start()
