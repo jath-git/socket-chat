233738ede554775e0b96cba7f74c470f0265cf84
Jathurshan Theivikaran <jathu@Jathurshans-MacBook-Pro.local>
2022-05-09 11:52:20 -0400

    restore server and add colour to console

--- classes/Queue.py
+++ classes/Queue.py
@@ -1,6 +1,7 @@
 import threading
-from helpers import get_long_time, clear_console, header
+from helpers import get_long_time, clear_console, header, console_colour_change
 from DatabaseService import DatabaseService
+import json
 
 class ServerMessage:
     def __init__(self, _summary, _time, _message):
@@ -28,27 +29,42 @@ class Queue:
         summary = summary.upper()
         self.__queue_list.append(ServerMessage(summary, self.__time, message))
 
-        print_str = ''
-        if summary == 'HEADING':
-            print_str = message
-        elif summary == 'OPTION':
-            print_str = f'[OPTION] {message}'
-        else:
-            print_str = f'[{summary}][{self.__time}] {message}'
+        print_str = self.get_print_str(summary, self.__time, message)
 
         print(print_str)
+
+    def get_print_str(self, summary, time, message):
+        if summary == 'HEADING':
+            return message
+        elif summary == 'OPTION':
+            return f'[{summary}] {message}'
+        else:
+            return f'[{summary}][{time}] {message}'
 
     def save(self):
         while len(self.__queue_list) > 0:
             top = self.pop(self.__queue_list)
             self.database_service.add_table(top.summary, top.time, top.message)
 
+    def print_row(self, row):
+        row_obj = {"id": row[0], "summary": row[1], "time": row[2], "message": row[3]}
+        print_str = self.get_print_str(row_obj['summary'], row_obj['time'], row_obj['message'])
+        print(print_str)
+
+
     def restore(self):
         clear_console()
-        header('Saved Server')
         rows = self.database_service.get_table()
-        for i in rows:
-            print(i)
+        if len(rows) > 0:
+            header('Saved Server')
+            print()
+            console_colour_change('green')
+            for i in rows:
+                self.print_row(i)
+            console_colour_change('black')
+        else:
+            self.push('CAUTION', 'No server data is saved')
+
 
     def clear(self):
         self.database_service.delete_table()
